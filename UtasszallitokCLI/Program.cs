e00ed25f2c29adc8fc5d764e8140cc70a6bd8279
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-02-05 09:30:23 +0100

    INIT

--- UtasszallitokCLI/Program.cs
+++ UtasszallitokCLI/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using UtasszallitokCLI;
+
+const string PATH = "D:\\PROJECTS\\CSHARP2025020501\\UTASSZALLITOK\\utasszallitok.txt";
+
+List<Utasszallito> utasszallitok = [];
+using StreamReader sr = new(PATH);
+_ = sr.ReadLine();
+while(!sr.EndOfStream) utasszallitok.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"4f: adatsorok szama: {utasszallitok.Count}");
---
+++ UtasszallitokCLI/Utasszallito.cs
@@ -0,0 +1,28 @@
+﻿namespace UtasszallitokCLI;
+
+class Utasszallito
+{
+    public string Tipus { get; set; }
+    public int Ev { get; set; }
+    public string Utas { get; set; }
+    public string Szemelyzet { get; set; }
+    public int Utazosebesseg { get; set; }
+    public int Felszallotomeg { get; set; }
+    public float Fesztav { get; set; }
+    public string Sebessegkategoria { get; set; }
+
+    public Utasszallito(string sor)
+    {
+        string[] tmp = sor.Split(';');
+
+        Tipus = tmp[0];
+        Ev = int.Parse(tmp[1]);
+        Utas = tmp[2];
+        Szemelyzet = tmp[3];
+        Utazosebesseg = int.Parse(tmp[4]);
+        Felszallotomeg = int.Parse(tmp[5]);
+        Fesztav = float.Parse(tmp[6]);
+
+        Sebessegkategoria = new Sebessegkategoria(Utazosebesseg).Kategorianev;
+    }
+}
